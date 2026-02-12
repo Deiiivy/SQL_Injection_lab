b354ccb63d2c6176a66c1c1506aedb97df5c7e51
Deiiivy <deivypr28@gmail.com>
2026-02-12 14:31:41 -0500

    add interface

--- index.js
+++ index.js
@@ -1,33 +1,47 @@
-const express = require('express');
-const sqlite3 = require('sqlite3').verbose();
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mysql = require("mysql2");
 
 const app = express();
+
+app.use(cors());
 app.use(bodyParser.json());
 
-const db = new sqlite3.Database(':memory:');
-
-db.serialize(() => {
-  db.run("CREATE TABLE users (email TEXT, password TEXT)");
-  db.run("INSERT INTO users VALUES ('admin@test.com','1234')");
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "deivy",
+  password: "deivy",
+  database: "sqli_lab",
 });
 
-app.post('/login', (req,res)=>{
+
+
+db.connect(err => {
+  if (err) throw err;
+  console.log("MariaDB Connected");
+});
+
+app.post("/login", (req, res) => {
+
   const { email, password } = req.body;
 
-  const query = `SELECT * FROM users WHERE email='${email}' AND password='${password}'`;
+  const query = `
+    SELECT * FROM users WHERE email='${email}' AND password='${password}'`;
 
   console.log(query);
 
-  db.all(query, [], (err, rows)=>{
-    if(rows.length>0){
-      res.json({success:true});
+  db.query(query, (err, result) => {
+    if (err) return res.status(500).json(err);
+
+    if (result.length > 0) {
+      res.json({ success: true });
     } else {
-      res.json({success:false});
+      res.json({ success: false });
     }
   });
 });
 
-app.listen(3000);
-console.log("Running on 3000");
-
+app.listen(3000, () => {
+  console.log("Running on 3000");
+});
